b521f788c6ef56868fedfae345bb3d6fdd4b2304
Maxim Klimov <Klimov.m.l@yandex.ru>
2018-08-16 22:42:22 +0300

    code cleanup

--- src/com/klimovinc/GooglePlayParser.java
+++ src/com/klimovinc/GooglePlayParser.java
@@ -1,16 +1,13 @@
 package com.klimovinc;
+
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class GooglePlayParser {
     private String developerID;
